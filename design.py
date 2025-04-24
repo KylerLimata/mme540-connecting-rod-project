8a6fc5d2327ca97f929a57d0801394c917085bae
KylerLimata <kyler.limata@gmail.com>
2025-04-24 15:38:48 -0700

    Stress at point 1

--- design.py
+++ design.py
@@ -2,17 +2,48 @@ import simulate as sim
 
 ## Define piston/engine parameters
 params = {
+    # Geometry Parameters
     'B': 0.08, # m
     'S': 0.096, # m
     'r': 0.2, # m
     'CR': 10,
+    'w_beam': 0.06, # m
+    't_beam': 0.04, # m
+    't_center': 0.02, # m
+    'w_base': 0.1, # m
+     # Stress Concentrations
+    'kt': {
+        'axial': [
+            1.0, # Points 1 and 2
+        ],
+        'bending': [
+            1.0, # Points 1 and 2
+        ]
+    },
+    # Otto Cycle Parameters
     'T1': 21, # Celcius
     'P1': 101.325*(10**3), # Pa
     'T3': 1871 # Celcius
 }
 
 ## Stress functions
-funcs = []
+def stress_at_1(params, Fx, Fy):
+    kt_axial = params['kt']['axial'][0]
+    kt_bending = params['kt']['bending'][0]
+    w_beam = params['w_beam']
+    t_beam = params['t_beam']
+    r_rod = params['r']
+    w_base = params['w_base']
+
+    A_cross = w_beam*t_beam
+    M = Fx*(r_rod - w_base)
+    sigma_x = 1.5*(Fx/A_cross)
+    sigma_y = kt_axial*(Fy/A_cross)
+    tau_xy = kt_bending*((6*M)/(t_beam*w_beam**2))
+
+    return sigma_x, sigma_y, tau_xy
+
+funcs = [stress_at_1]
 
 ## 
 npoints = 200
