ced1bc1995508c9b3fad724671c1b5534c748822
RedstoneParadox <kyler.limata@gmail.com>
2025-05-07 20:06:58 -0700

    Final final design

--- revised_design.py
+++ revised_design.py
@@ -86,8 +86,61 @@ def stress_at_2_revised(params, Fx, Fy):
 
     return sigma_x, sigma_y, tau_xy
 
-
 def stress_at_3(params, Fx, Fy):
+    kt_axial = params['kt']['axial'][2]
+    kt_bending = params['kt']['bending'][2]
+    w_beam = params['w_beam']
+    t_beam = params['t_beam']
+    w_web = params['w_web']
+    t_web = params['t_web']
+    r_rod = params['r']
+    w_base = params['w_base']
+    r_base_fillet = params['r_base_fillet']
+
+    t_hole = t_beam - t_web
+    A_total = w_beam*t_beam - 2*w_web*(t_beam - t_web)
+    A_hole = (w_beam - w_web)*(t_beam - t_web)
+    percent_depth = A_hole/A_total
+    M = (r_rod - w_base/2 - r_base_fillet - w_web/2)*Fx
+    I_mid = (t_web*w_beam**3)/12
+    c = w_beam/2
+    
+    sigma_x = kt_bending*((percent_depth*6*M)/((w_beam - w_web)*t_hole**2)) \
+                + ((1 - percent_depth)*M*c)/I_mid
+    sigma_y = kt_axial*(percent_depth*Fy)/((w_beam - w_web)*t_hole) \
+                + ((1 - percent_depth)*Fy)/(w_beam - t_web)
+    tau_xy = (1.5*Fx)/(w_beam*t_web)
+
+    return sigma_x, sigma_y, tau_xy
+
+def stress_at_3_revised(params, Fx, Fy):
+    kt_axial = params['kt']['axial'][2]
+    kt_bending = params['kt']['bending'][2]
+    w_beam = params['w_beam']
+    t_beam = params['t_beam']
+    w_web = params['w_web']
+    t_web = params['t_web']
+    r_rod = params['r']
+    w_base = params['w_base']
+    r_base_fillet = params['r_base_fillet']
+    r_web_fillet = params['r_web_fillet']
+
+    A_fillet = (1 - 0.25*np.pi)*r_web_fillet**2
+    A_total = w_beam*t_beam - w_web*(t_beam - t_web) + 4*A_fillet
+    A_hole = (w_beam - w_web)*(t_beam - t_web)
+    concentrated_percent = A_hole/A_total
+
+    M = (r_rod - w_base/2 - r_base_fillet - w_web/2)*Fx
+    I_mid = (t_web*w_beam**3)/12
+    
+    sigma_x = kt_bending*concentrated_percent*((M*w_beam)/(2*I_mid))
+    sigma_y = kt_axial*(concentrated_percent*Fy)/(A_hole)
+    tau_xy = (1.5*Fx)/(w_beam*t_web)
+
+    return sigma_x, sigma_y, tau_xy
+
+
+def stress_at_4(params, Fx, Fy):
     w_beam = params['w_beam']
     t_beam = params['t_beam']
     w_web = params['w_web']
@@ -108,7 +161,7 @@ def stress_at_3(params, Fx, Fy):
 
     return sigma_x, sigma_y, tau_xy
 
-def stress_at_4(params, Fx, Fy):
+def stress_at_5(params, Fx, Fy):
     kt_axial = params['kt']['axial'][4]
     d_pin = params['d_pin']
     d_out = params['d_out']
@@ -122,7 +175,7 @@ def stress_at_4(params, Fx, Fy):
 
     return sigma_x, sigma_y, tau_xy
 
-funcs = [stress_at_1, stress_at_2_revised, stress_at_3, stress_at_4]
+funcs = [stress_at_1, stress_at_2_revised, stress_at_3_revised, stress_at_4, stress_at_5]
 
 ## 
 npoints = 200
