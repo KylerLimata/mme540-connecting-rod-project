1f839c3c66dde1c40f264d216592839855491ac1
RedstoneParadox <kyler.limata@gmail.com>
2025-06-11 20:08:01 -0700

    Fix missing stress concentrations

--- redesign.py
+++ redesign.py
@@ -9,7 +9,6 @@ params = {
     'r': 0.2, # m, connecting rod length
     'CR': 10, # compression ratio
     'd_pin': 0.045, # m, pin diameter
-    'd_ring': 0.06, # m
     't_ring': 0.06,  # m
     't_beam': 0.03, # m, outer length of the beam
     'w_web': 0.03, # m, width of the web 
@@ -19,14 +18,14 @@ params = {
         'axial': [
             1.65, # Point 1
             1.3, # Point 2
-            1.0, # Point 3
-            1.0, # Point 4, not used
+            1.0, # Point 3, not used
+            2.4, # Point 4
         ],
         'bending': [
             1.35, # Point 1
             1.0, # Point 2, not used
-            1.0, # Point 3
-            1.0, # Point 4
+            1.0, # Point 3, not used
+            1.2, # Point 4
         ]
     },
     # Otto Cycle Parameters
@@ -52,10 +51,14 @@ rbasefillet_over_wbeam = 0.3
 params['w_beam'] = params['w_base']/wbase_over_wbeam # m, width of the beam section
 params['r_base_fillet'] = params['w_beam']*rbasefillet_over_wbeam # m, fillet where the beam meets base
 
+dpin_over_dring = 0.6
+params['d_ring'] = params['d_pin']/dpin_over_dring # Outer diameter of the ring around the pin
+
 print(f"t_web = {params['t_web']}")
 print(f"r_web_fillet = {params['r_web_fillet']}")
 print(f"w_beam = {params['w_beam']}")
 print(f"r_base_fillet = {params['r_base_fillet']}")
+print(f"d_ring = {params['d_ring']}")
 
 ## 
 npoints = 100
